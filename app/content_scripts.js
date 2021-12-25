c3202dc0323116b37e3dfdd7947d14ea43b0c29b
飯沼 秀明 <work@web-rice.com>
2021-12-25 19:09:55 +0900

    ターゲット要素追加

--- app/content_scripts.js
+++ app/content_scripts.js
@@ -6,9 +6,16 @@ const hideClass = 'yne_hide';
 const keyName = 'yne_words';
 if (document.domain === 'www.yahoo.co.jp') {
   targets = '#Topics li, #Topics article > a, #Stream article';
+  // eslint-disable-next-line max-len
+} else if (document.domain === 'sports.yahoo.co.jp' || document.domain === 'baseball.yahoo.co.jp' || document.domain === 'soccer.yahoo.co.jp' || document.domain === 'keiba.yahoo.co.jp') {
+  // eslint-disable-next-line max-len
+  targets = '.lsn-listPickup__item,#pic_photo,.cm-topTimeLine__item,.sn-info__item,.cm-videoEmbedSub,.cm-list__item,.sn-doPickup,.sn-doArticleList__item,.sn-textList__item,.sn-list__item,.sn-pickup__item,.io-pickup__item,.io-list__item,.sn-videoList__item,.bb-timeLine__item,#yjSMPickup,.yjMS > li,.sc-timeLine__item';
+  // eslint-disable-next-line no-dupe-else-if
+} else if (document.domain === 'keiba.yahoo.co.jp') {
+  targets = '.mgnBS,.glanceArticleBox > li';
 } else {
   // eslint-disable-next-line max-len
-  targets = '.listModBoxWrap,.listPaneltype_cont, .listFeedWrap, .topicsListItem, .yjnSub_list_item, .newsFeed_item, #newsTopics li, #ranking li';
+  targets = '.listModBoxWrap,.listPaneltype_cont, .listFeedWrap, .topicsListItem, .yjnSub_list_item, .newsFeed_item, #newsTopics li, #ranking li,.subList_item,[data-ual-view-type="list"],.yjnSubTopics_list_item,section.topics > div > p';
 }
 
 chrome.storage.local.get([keyName], function(result) {
@@ -18,7 +25,7 @@ chrome.storage.local.get([keyName], function(result) {
 });
 
 loop();
-setInterval(loop, 500);
+setInterval(loop, 1000);
 
 /**
  * wordsを一定間隔で処理する関数
